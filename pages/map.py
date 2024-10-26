88b4af32e6091bc87e71ec65f1ad21fadfdd5422
maggie <waterlilypearl@gmail.com>
2024-10-26 18:17:43 -0400

    map set up and correct connection to supabase in main

--- pages/map.py
+++ pages/map.py
@@ -1,4 +1,19 @@
 import streamlit as st
-st.title("Map")
+import os
+from supabase import Client, create_client
+from dotenv import load_dotenv
+
+load_dotenv()
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+
+response = supabase.table("input_data").select("latitude").execute()
+print(response)
+
+
+st.title("Survivors Map")
 
 st.map()
